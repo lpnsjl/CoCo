e48c7a46d66fc0b3191e6a9fe7cd0d1cc6378952
lpnsjl <1752128872@qq.com>
2018-11-26 20:31:38 +0800

    卷基层

--- activation.py
+++ activation.py
@@ -2,11 +2,14 @@
 
 import numpy as np
 
+
 class Sigmoid(object):
     """
     sigmoid激活函数
     """
-    def forward(self, z):
+
+    @staticmethod
+    def forward(z):
         """
         前向传播
         :param z:
@@ -14,10 +17,28 @@ class Sigmoid(object):
         """
         return 1/(1+np.exp(-z))
 
-    def backward(self, input):
+    @staticmethod
+    def backward(output):
         """
-        反向传播,
-        :param input: 连接层的输入
+        反向传播
+        :param output: 连接层的输入
         :return:
         """
-        return input*(1-input)
+        return output*(1-output)
+
+
+class Relu(object):
+    """
+    Relu激活函数
+    """
+
+    @staticmethod
+    def forward(z):
+        z[z <= 0] = 0
+        return z
+
+    @staticmethod
+    def backward(output):
+        output[output <= 0] = 0
+        output[output > 0] = 1
+        return output
